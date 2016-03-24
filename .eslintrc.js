64626cc845bd09a159cd4714f3f05fa8c47cc838
Alvaro Pinot <alvaro@neatnait.com>
2016-03-24 13:53:44 +0100

    chore(scaffold): add project structure

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "google",
+
+    "rules": {
+      "no-unused-expressions": [
+        2,
+        {
+          allowShortCircuit: true,
+          allowTernary: true
+        }
+      ],
+      "space-before-function-paren": [2, "always"]
+    }
+};
