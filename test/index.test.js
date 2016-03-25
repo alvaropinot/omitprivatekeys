6fcef3641eb85c4c35e82a1ab41b427034dfb1c9
juliomatcom <juliomatcom@gmail.com>
2016-03-25 10:51:37 +0000

    added new tests

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,24 @@ test('omitPrivateKeys must be defined', t => {
   t.end();
 });
 
-test('should clean keys preceeded with `_`', t => {
+test('if convention is defined and is not an instance of RegExp', t => {
+  const obj = { a : 1, _b : 2 };
+  var wrongConventions = ['', 1, false, null, {}];
+
+  wrongConventions.forEach(function (elm) {
+    t.throws(() => omitPrivateKeys(obj, elm), undefined, 'Should throw an error if convention is ' + typeof elm);
+  })
+  t.end();
+});
+
+test('if convention is an instance of RegExp', t => {
+    const obj = { a : 1, _b : 2 };
+    // ok
+    t.doesNotThrow(() => omitPrivateKeys(obj, new RegExp('^\$\$')));
+    t.end();
+});
+
+test('if no convention passed should clean keys preceeded with `_`', t => {
   const obj = {
     _foo: 'foo',
     _bar: 'bar',
@@ -28,7 +45,7 @@ test('should clean keys preceeded with `_`', t => {
   t.end();
 });
 
-test('should not clean keys containing `_` other positions than start', t => {
+test('if no convention passed should not clean keys containing `_` other positions than start', t => {
   // Disable eslint rule for this test only.
   /* eslint camelcase:[0] */
   const obj = {
@@ -45,6 +62,33 @@ test('should not clean keys containing `_` other positions than start', t => {
   t.deepEqual(
     actual, expected,
     'should return a object with keys containing `_`'
+  );
+  t.end();
+});
+
+test('if convention is passed should clean keys that match the RegExp', t => {
+  const obj = {
+    _foo: 'foo',
+    $$bar: 'bar',
+    bar$$ : 'isFine',
+    a: 2,
+    b: 'b',
+    c: true,
+    $$cuba : 1
+  };
+  const actual = omitPrivateKeys(obj, /^\$\$/);
+
+  const expected = {
+    _foo: 'foo',
+    bar$$ : 'isFine',
+    a: 2,
+    b: 'b',
+    c: true
+  };
+
+  t.deepEqual(
+    actual, expected,
+    'should return a object without keys matched by `/^\\$\\$/`'
   );
   t.end();
 });
