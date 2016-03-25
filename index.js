d080c2bc3cebc770b6c02a08b1bad1c737b4478b
juliomatcom <juliomatcom@gmail.com>
2016-03-25 10:51:00 +0000

    Add support to an additional parameter for others conventions

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 /**
  * Picks objects keys by applying a predicate function.
  * @param  {Function} predicate The function applied for each key.
- * @return {Function} Returns the partially applied function.
+ * @return {Object} Returns the objects key.
  */
-function pickKeysBy (predicate) {
-  return function (originalObject) {
-    return Object.keys(originalObject)
-      .reduce((finalObject, key) => {
-        predicate(key) && (finalObject[key] = originalObject[key]);
-        return finalObject;
-      },
-    {});
-  };
+function pickKeysBy (originalObject, predicate) {
+  return Object.keys(originalObject)
+    .reduce((finalObject, key) => {
+      predicate(key) && (finalObject[key] = originalObject[key]);
+      return finalObject;
+    },
+  {});
 }
 
-// Check if a given key starts with `_`.
-const _isNotPrivate = key => key[0] !== '_';
-const omitPrivateKeys = pickKeysBy(_isNotPrivate);
+const omitPrivateKeys = function (originalObject, convention) {
+  var newObj;
+  if (convention === undefined) {
+    newObj = pickKeysBy(originalObject, (key) => key[0] !== '_');// Check if a given key starts with `_`.
+  }
+  else if (convention instanceof RegExp) {
+    newObj = pickKeysBy(originalObject, (key) => !convention.test(key));//Check if key match with convention
+  }
+  else {
+    throw new Error('convention argument must be an instace of RegExp if defined');
+  }
+  return newObj;
+};
 
 module.exports = omitPrivateKeys;
